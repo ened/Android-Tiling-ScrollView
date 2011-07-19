c6c387f93489a447d707352cb791a5f0788af2de
Sebastian Roth <sebastian.roth@gmail.com>
2011-07-19 12:58:25 +0800

    Added sample project.

---
+++ sample/src/asia/ivity/android/tiledscrollview/TiledScrollViewSample.java
@@ -0,0 +1,14 @@
+package asia.ivity.android.tiledscrollview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TiledScrollViewSample extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
