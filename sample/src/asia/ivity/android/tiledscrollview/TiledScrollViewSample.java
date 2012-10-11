7d604bf733a29bb08f989b456da4bd58a716842b
Sebastian Roth <sebastian.roth@gmail.com>
2012-10-11 10:25:31 +0800

    Reorganized the directories, made sure the sample works again and fixed #8.

--- sample/src/asia/ivity/android/tiledscrollview/TiledScrollViewSample.java
+++ sample/src/asia/ivity/android/tiledscrollview/TiledScrollViewSample.java
@@ -3,8 +3,7 @@ package asia.ivity.android.tiledscrollview;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TiledScrollViewSample extends Activity
-{
+public class TiledScrollViewSample extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
